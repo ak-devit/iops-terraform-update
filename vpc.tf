56bafa4f2dedcccb4f9a21b86216b412dfd072d4
Andriy Kunitskyy <114353305+ak-devit@users.noreply.github.com>
2022-12-23 12:10:53 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,9 @@
 
-#=====================================================================
+
 #Creating VPC - iops-terraformvpc only local terraform name
-#=====================================================================
 
 resource "aws_vpc" "iops-terraformvpc" {
-  cidr_block = var.aws_vpc_cidr #65,536
+  cidr_block = var.aws_vpc_cidr 
 
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -22,8 +21,7 @@ resource "aws_subnet" "iops-PublicSubnet-A" {
   vpc_id            = aws_vpc.iops-terraformvpc.id
   cidr_block        = var.PublicSubnet-A_cidr #256
   availability_zone = var.availability_zone_a
-  #- (Optional) Specify true to indicate that instances 
-  #launched into the subnet should be assigned a public IP address.
+  
   map_public_ip_on_launch = true
 
   tags = {
@@ -35,8 +33,7 @@ resource "aws_subnet" "iops-PublicSubnet-B" {
   vpc_id            = aws_vpc.iops-terraformvpc.id
   cidr_block        = var.PublicSubnet-B_cidr
   availability_zone = var.availability_zone_b
-  #- (Optional) Specify true to indicate that instances 
-  #launched into the subnet should be assigned a public IP address.
+  
   map_public_ip_on_launch = true
 
   tags = {
@@ -77,9 +74,7 @@ resource "aws_internet_gateway" "iops-terraform-ig" {
 }
 
 #Route table to route trafic from our subnet to internet gateway
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-#The destination for the route is 0.0.0.0/0, which represents all IPv4 addresses. 
-#The target is the internet gateway that's attached to VPC.
+
 resource "aws_route_table" "iops_terraform_rtb_pub" {
   vpc_id = aws_vpc.iops-terraformvpc.id
 
@@ -88,10 +83,6 @@ resource "aws_route_table" "iops_terraform_rtb_pub" {
     cidr_block = var.aws_route_table_cidr
     gateway_id = aws_internet_gateway.iops-terraform-ig.id
   }
-
-  #local target
-  #The default route, mapping the VPC's CIDR block to "local", 
-  #is created implicitly and cannot be specified.
 
   tags = {
     Name = var.aws_route_table_tags
@@ -106,14 +97,9 @@ resource "aws_route_table" "iops_terraform_rtb_private" {
   }
 }
 
-#==============================================================
-#Route table association
-#Asociation b/w a route table and a subnet
-#Provides a resource to create an association between a route 
-#table and a subnet or a route table and an internet gateway or
-# virtual private gateway.
 
-#===============================================================
+#Route table association
+
 #public a
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.iops-PublicSubnet-A.id
@@ -138,17 +124,5 @@ resource "aws_route_table_association" "private_b" {
 }
 
 
-
-# #aws_main_route_table_association
-# resource "aws_default_route_table" "terr_public_assosiation" {
-#   vpc_id         = aws_vpc.terr_vpc.id
-#   default_route_table_id = aws_route_table.terr_rtb_public.id
-# }
-
-# #aws_main_route_table_association
-# resource "aws_default_route_table" "terr_private_assosiation" {
-#   vpc_id         = aws_vpc.terr_vpc.id
-#   default_route_table_id = aws_route_table.terr_rtb_private.id  #route_table_id instead if aws_main_route_table_association is used
-# }
 
 
